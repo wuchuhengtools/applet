216fbd450505968e5a6ec8bd2d0f0ac6abc57d75
wuchuheng@163.com <wuchuheng@163.com>
2018-12-19 00:59:09 +0800

    继续开发小程序

--- conf/config.php
+++ conf/config.php
@@ -245,13 +245,13 @@ return [
         // 数据库连接DSN配置
         'dsn'             => '',
         // 服务器地址
-        'hostname'        => '127.0.0.1',
+        'hostname'        => 'mysql',
         // 数据库名
         'database'        => 'applet',
         // 数据库用户名
         'username'        => 'root',
         // 数据库密码
-        'password'        => 'root',
+        'password'        => '123456',
         // 数据库连接端口
         'hostport'        => '',
         // 数据库连接参数
